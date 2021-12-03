1cd849fd0bdc7c6bcd8a1dfce00476f35a07ad6d
Jemstefanny <92009785+Jemstefanny@users.noreply.github.com>
2021-12-02 20:36:25 -0600

    Add files via upload

---
+++ resources/views/index.blade.php
@@ -0,0 +1,68 @@
+@extends('layouts.app')
+@section('content')
+
+<div class="imagen-importar grid grid-cols-1 m-auto">
+
+    <div class="flex text-gray-100 pt-10">
+
+        <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center">
+
+            <h1 class="sm:text-white text-6xl uppercase font-extrabold text-shadow-md pb-14">
+               NAVIERA PEP
+            </h1>
+            <h1 class="sm:text-white text-5xl  font-bold font-serif text-shadow-md pb-14">
+                Make it easy!!
+             </h1>
+            <a href="" class="text-center bg-gray-50 text-gray-700 py-2 px-4 font-bold text-xl uppercase">
+                Read More
+            </a>
+        </div>
+    </div>
+</div>
+
+<div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
+    <div>
+        <img src="https://cdn.pixabay.com/photo/2017/12/15/22/25/hamburg-3021820_960_720.jpg" width="700" alt="">
+    </div>
+
+    <div class="m-auto sm:m-auto text-left w-4/5 block">
+        <h2 class="text-4xl font-extrabold text-cool-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit necessitatibus delectus
+        </h2>
+
+        <p class="py-8 text-gray-500 text-s">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. , non atque recusandae?
+        </p>
+
+        <p class="font-extrabold text-gray-600 text-s pb-9">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ratione beatae totam omnis placeat ex, hic doloribus, a fuga explicabo dolorum quod earum dignissimos ducimus tempore rerum. Fugit, ullam! Illum!
+        </p>
+
+        <a href="blog" class="uppercase bg-blue-500 text-gray-100 text-s font-extrabold py-3 px-8 rounded-xl">
+            View Blogs
+        </a>
+    </div>
+</div>
+
+<div class="text-center pb-15 bg-gray-900 text-white">
+    <h2 class="text-2xl p-5 text-l">
+        I´m expert...
+    </h2>
+
+    <span class="font-extrabold block text-4xl py-1">
+        Lorem, ipsum.
+    </span>
+
+    <span class="font-extrabold block text-4xl py-1">
+        Lorem, ipsum.
+    </span>
+
+    <span class="font-extrabold block text-4xl py-1">
+        Lorem, ipsum.
+    </span>
+
+    <span class="font-extrabold block text-4xl py-1">
+        Lorem, ipsum.
+    </span>
+</div>
+@endsection
---
+++ storage/framework/views/7ceb5a3c446cfd6652fa6459013683b60e2fe70c.php
@@ -0,0 +1,95 @@
+<footer class="bg-gray-900 py-20 mt-20">
+    <div class="sm:grid grid-cols-3 w-4/5 pb-10 m-auto border-b-2 border-gray-700">
+        <div>
+            <h3 class="text-1, sm:font bold text-gray-100">Pages</h3>
+            <ul class="py-4 sm:text-s pt-4 text-gray-100">
+                <li class="pb-1">
+                    <a href="/">
+                        Home
+                    </a>
+                </li>
+
+                <li>
+                    <a href="/Blog">
+                    Servicios e Informacion
+                    </a>
+                </li>
+
+                <li>
+                    <a href="/Login">
+                    Login
+                    </a>
+                </li>
+
+                <li>
+                    <a href="/Register">
+                    Register
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div>
+            <h3 class="text-1, sm:font bold text-gray-100">Pages</h3>
+            <ul class="py-4 sm:text-s pt-4 text-gray-100">
+                <li class="pb-1">
+                    <a href="/">
+                    About us
+                    </a>
+                </li>
+
+                <li>
+                    <a href="/Reservations">
+                    Reservations.
+                    </a>
+                </li>
+
+                <li>
+                    <a href="/Itinerarios">
+                        Itinerarios.
+                    </a>
+                </li>
+
+                <li>
+                    <a href="/Documentation">
+                    Lorem, ipsum.
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div>
+            <h3 class="text-1, sm:font bold text-gray-100">Pages</h3>
+            <ul class="py-4 sm:text-s pt-4 text-gray-100">
+                <li class="pb-1">
+                    <a href="/">
+                    Find us
+                    </a>
+                </li>
+
+                <li>
+                    <a href="/Quality Promises">
+                    Quality Promises.
+                    </a>
+                </li>
+
+                <li>
+                    <a href="/Navigator">
+                        Navigator.
+                    </a>
+                </li>
+
+                <li>
+                    <a href="/Register">
+                    Lorem, ipsum.
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <p class="w-25 w-4/5 pb-3 m-auto text-xs text-gray-100 pt-6">
+        Copyright 2021 Naviera Pep. All right reserved.
+    </p>
+</footer>
+<?php /**PATH C:\xampp\htdocs\demo\resources\views/layouts/footer.blade.php ENDPATH**/ ?>
